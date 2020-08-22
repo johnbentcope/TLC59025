fb8d52d9576fe5d727f7afe4c2ce259d84757b83
John Cope <johnbentcope@gmail.com>
2020-08-22 18:21:18 -0400

    CCS versions are fighting

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -2,13 +2,11 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-SHELL = cmd.exe
-
 # Each subdirectory must supply rules for building sources it contributes
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccs1010/ccs/ccs_base/msp430/include" --include_path="C:/Users/johnb_cux15br/workspace_v10/TLC59025" --include_path="C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include" --advice:power=all --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"/home/john/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="/home/john/ti/ccs1010/ccs/ccs_base/msp430/include" --include_path="/home/john/repos/TLC59025" --include_path="/home/john/ti/ccs1010/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include" --advice:power=all --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
